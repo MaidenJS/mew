b24a2aa69cdf20b5e045b62d705316b273601c5f
yichen <yichenzhu1337@gmail.com>
2016-10-15 17:17:27 -0400

    new updates

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+(function ($) {
+    $(document).ready(function () {
+
+        $('.profile-image').on('mouseover', function () {
+            $('.settings').stop().toggle(); //css('display', 'none');
+        });
+
+        $('.profile-image').focusout(function () {
+            $('.settings').hide();
+        });
+    });
+})(jQuery);
