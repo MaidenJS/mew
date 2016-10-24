94c6e51c2ac5c6d202eb1fc2b46a7e7ee7d4d8e7
yichen <yichenzhu1337@gmail.com>
2016-10-23 20:09:49 -0400

    new updates

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 (function ($) {
     $(document).ready(function () {
 
-        $('.profile-image').on('mouseover', function () {
-            $('.settings').stop().toggle(); //css('display', 'none');
+        // Nav Bar settings
+        var nav_settings = $('.navbar-container__nav-settings');
+        $(document).on('click', function() {
+            if (nav_settings.css('display') == 'block') {
+                nav_settings.css('display', 'none');
+            }
+        });
+        $('.navbar-container__profile-image').on('click', function(event) {
+            nav_settings.toggle();
+            console.log('asdas');
+            event.stopPropagation();
         });
 
-        $('.profile-image').focusout(function () {
-            $('.settings').hide();
-        });
     });
 })(jQuery);
